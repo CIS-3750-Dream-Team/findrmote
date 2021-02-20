7f48e3f31ff8f3a43465910f4ea5f4546584aa44
AJ F <aferron@uoguelph.ca>
2021-02-19 19:24:32 -0500

    Create UI server file structure

---
+++ app/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './js/App';
+
+import 'bootstrap/scss/bootstrap.scss';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ app/src/js/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import '../scss/main.scss';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'> Hello World! </div>
+    );
+  }
+}
+
+export default App;
