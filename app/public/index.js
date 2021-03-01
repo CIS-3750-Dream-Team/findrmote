a2062fd8afdc6917217820c26ad2d907b20cb5f9
AJ F <aferron@uoguelph.ca>
2021-02-28 19:56:37 -0500

    Make Dropdown component

---
+++ app/public/index.js
@@ -0,0 +1,23 @@
+/* index.js
+ * February 28, 2021
+ * Extra event handlers
+ */
+
+
+$(() => {
+  // Handlers for multi-level Dropdown component
+  // https://stackoverflow.com/questions/44467377/bootstrap-4-multilevel-dropdown-inside-navigation
+  $('.dropdown-menu .dropdown-toggle').on('click', function(e) {
+    if (!$(this).next().hasClass('show')) {
+      $(this).parents('.dropdown-menu').first().find('.show').removeClass('show');
+    }
+
+    $(this).next('.dropdown-menu').toggleClass('show');
+
+    $(this).parents('.dropdown.show').on('hidden.bs.dropdown', function(e) {
+      $('.dropdown-submenu .show').removeClass('show');
+    });
+
+    return false;
+  });
+})
