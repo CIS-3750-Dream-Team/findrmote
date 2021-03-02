abb40ab847803343e1ce78f02e887bd8102a5c6b
Niloy <niloykumar256@gmail.com>
2021-03-01 21:41:42 -0500

    COMMIT#4 - BuildJobCardComponent
    Adjusted code for new home layout; all cards have same height;

--- app/src/js/App.js
+++ app/src/js/App.js
@@ -1,31 +1,39 @@
 import React from "react";
 import { Route, Link, BrowserRouter as Router } from "react-router-dom";
 
-import Home from './pages/Home';
-import Components from './pages/Components';
+import Home from "./pages/Home";
+import Components from "./pages/Components";
 
 function App(props) {
-  return (
-    <Router>
-      <div className='App p-5'>
-        <header>
-          {/* Replace all this (Issue #4) */}
-          <h1>findrmote</h1>
-          <nav className='nav flex-column align-items-start w-25 my-3'>
-            <Link className='btn btn-link' to='/home'> Home </Link>
-            <Link className='btn btn-link' to='/components'> Components </Link>
-          </nav>
-        </header>
+	return (
+		<Router>
+			<div className="App container">
+				<header>
+					{/* Replace all this (Issue #4) */}
+					<h1>findrmote</h1>
+					<nav className="nav flex-column align-items-start w-25 my-3">
+						<Link className="btn btn-link" to="/home">
+							Home
+						</Link>
+						<Link className="btn btn-link" to="/components">
+							Components
+						</Link>
+					</nav>
+				</header>
 
-        <Route exact path='/home' render={_ => <Home />} />
-        <Route exact path='/components' render={_ => <Components />} />
+				<Route exact path="/home" render={(_) => <Home />} />
+				<Route
+					exact
+					path="/components"
+					render={(_) => <Components />}
+				/>
 
-        <footer>
-          {/* No footer has been design yet. Do we need/want one? */}
-        </footer>
-      </div>
-    </Router>
-  );
+				<footer>
+					{/* No footer has been design yet. Do we need/want one? */}
+				</footer>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
--- app/src/js/pages/Home.js
+++ app/src/js/pages/Home.js
@@ -1,8 +1,9 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
+import JobCard from "../components/JobCard";
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-// delete after connecting to api later. backend api doesn't send proper job_data format yet
+// delete after connecting to api later. backend api doesn't send proper job_data format yet: no tags array
 const job_datas = [
 	{
 		job_id: "1234",
@@ -45,35 +46,44 @@ const job_datas = [
 ];
 
 function Home(props) {
-  useEffect(() => {
-    // Get job data on component render
-    fetch(`${API_URL}/jobs`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(res => res.json())
-      .then(job_data => {
-        // Store the job data in state
-        console.log(job_data);
-      })
-      .catch(err => {
-        // Display an error to user, tell them to refresh
-        console.error(err);
-      })
-  })
+	useEffect(() => {
+		// Get job data on component render
+		fetch(`${API_URL}/jobs`, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+			.then((res) => res.json())
+			.then((job_data) => {
+				// Store the job data in state
+				console.log(job_data);
+			})
+			.catch((err) => {
+				// Display an error to user, tell them to refresh
+				console.error(err);
+			});
+	});
 
-  return (
-    <div id='home'>
-      <div className="criteria-wrapper">
-        {/* Add the SortSelect, FilterSelect, and SearchBar here */}
-      </div>
-      <div className="job-wrapper">
-        {/* Add the JobCard mapping here */}
-      </div>
-    </div>
-  );
+	return (
+		<div id="home">
+			<div className="criteria-wrapper">
+				{/* Add the SortSelect, FilterSelect, and SearchBar here */}
+			</div>
+
+			{/* JobCard Container: Renders Four jobs in each row */}
+			<div className="job-wrapper row my-4">
+				{job_datas.map((job_data) => (
+					<div
+						className="col-12 col-md-4 col-xl-3"
+						key={job_data.job_id}
+					>
+						<JobCard job_data={job_data} />
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default Home;
