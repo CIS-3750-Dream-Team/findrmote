ab013d759ca0c7d64049d7b6755e9e99a2f8d517
AJ F <aferron@uoguelph.ca>
2021-03-22 21:20:53 -0400

    Move demo jobs from React app to API

--- api/routes/jobs.js
+++ api/routes/jobs.js
@@ -11,9 +11,33 @@ module.exports = {
       data: [
         {
           job_id: '1234',
-          company: 'Test Company',
+          company: 'Google',
           job_title: 'Software Developer',
-          description: 'Test description.',
+          tags: ['Firebase', 'Javascript', 'HTML', 'Github', 'Python', 'Golang', 'Bash'],
+        },
+        {
+          job_id: '5678',
+          company: 'Tulip Retail',
+          job_title: 'Frontend Developer',
+          tags: ['jQuery', 'Javascript', 'HTML', 'CSS', 'Github'],
+        },
+        {
+          job_id: '9888',
+          company: 'NCR',
+          job_title: 'Software Developer',
+          tags: ['Java', 'Github', 'HTML', 'Unit Testing', 'Git'],
+        },
+        {
+          job_id: '9995',
+          company: 'The Cooperators',
+          job_title: 'Fullstack Developer',
+          tags: ['CSS', 'Javascript', 'Github', 'HTML', 'Git', 'Kafka', 'Python'],
+        },
+        {
+          job_id: '9699',
+          company: 'Mozilla',
+          job_title: 'Quality Assurance',
+          tags: ['Github', 'Javascript'],
         }
       ]
     });
