aa1bb26d1d5afa7bfda92bc83670c59b21bc953b
AJ Ferron <45947696+aj-ferr@users.noreply.github.com>
2021-03-24 20:58:50 -0400

    task/add nav bar (#25)
    * Add nav bar to home page
    
    * Add login/register buttons to nav bar

--- app/src/js/App.js
+++ app/src/js/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import { HiOutlineSun, HiOutlineBell, HiOutlineUserCircle } from "react-icons/hi";
+import { IconContext } from "react-icons";
 
 import Home from './pages/Home';
 import Components from './pages/Components';
@@ -8,20 +10,44 @@ function App(props) {
   return (
     <Router>
       <div className="App container">
-        <header className="row mx-4 mx-sm-2 mt-5">
-          {/* Replace all this (Issue #4) */}
-          <h1 className="col">findrmote</h1>
-          <nav className="nav col align-items-center justify-content-end w-25 ">
-            <Link className="btn btn-link" to="/home"> Home </Link>
-            <Link className="btn btn-link" to="/components"> Components </Link>
-          </nav>
+        <header className="row justify-content-center">
+          <div className="col-sm col-10 mt-5">
+            <div className="row align-items-center justify-content-between mt-sm-5 mt-3">
+              <Link className="col" to="/">
+                <img id="logo" src="logo.svg" alt=""/>
+              </Link>
+
+              <div className="col d-md-flex d-none justify-content-end me-5">
+                <Link className="btn btn-light" to="/login"> Sign in </Link>
+                <Link className="btn btn-secondary ms-3" to="/register"> Register </Link>
+              </div>
+
+              <div className="col-5 col-xl-2 col-md-3">
+                <div className="d-flex flex-row justify-content-between">
+                  <IconContext.Provider value={{color: '#5a6570'}} >
+                    <Link className="p-0" to="/"> <HiOutlineSun size={30} /> </Link>
+                    <Link className="p-0" to="/register"> <HiOutlineBell size={30} /> </Link>
+                    <Link className="p-0" to="/profile"> <HiOutlineUserCircle size={30} /> </Link>
+                  </IconContext.Provider>
+                </div>
+              </div>
+            </div>
+          </div>
         </header>
 
-        <Route exact path="/home" render={(_) => <Home />} />
+        {/* Replace 'undefined' as pages are created */}
+        <Route exact path="/" render={(_) => <Home />} />
+        <Route exact path="/login" render={(_) => undefined} />
+        <Route exact path="/register" render={(_) => undefined} />
+        <Route exact path="/profile" render={(_) => undefined} />
+        <Route exact path="/job:job_id" render={(_) => undefined} />
+        <Route exact path="/admin" render={(_) => undefined} />
+
+        {/* For development. Displays some common components/elements/patterns */}
         <Route exact path="/components" render={(_) => <Components />} />
 
         <footer>
-          {/* No footer has been design yet. Do we need/want one? */}
+          {/* No footer has been designed yet. Do we need/want one? */}
         </footer>
       </div>
     </Router>
