a3222949c10d77c3f87b0d4e10c42f94c64ff745
Niloy <niloykumar256@gmail.com>
2021-03-27 15:14:35 -0400

    Dynamic size in Job controls

--- app/src/js/components/JobControl.js
+++ app/src/js/components/JobControl.js
@@ -17,9 +17,12 @@ const { useState, useEffect } = React;
 // TODO: create a tooltip component or use react-tooltip and wrap the controls around the tooltip. Team discussion required
 
 /**
+ * @param {Object} props The properties sent to the component
+ * @param {Object} props.size The size of the icons. Default is 1em
  * @returns {ReactElement} The Job Card component with job title, company, job details.
  */
-export default function () {
+export default function (props) {
+  const size = props.size ?? '1em';
   const [jobID, setJobID] = useState('');
   const [heart, setheart] = useState(false);
   const [bookmark, setBookmark] = useState(false);
@@ -43,22 +46,38 @@ export default function () {
     <div className='row'>
       <div className='col-3'>
         <button className='job-control--button' onClick={handleHeart}>
-          {heart ? <HiHeart color='#ef4444' /> : <HiOutlineHeart color='#ef4444' />}
+          {heart ? (
+            <HiHeart size={size} color='#ef4444' />
+          ) : (
+            <HiOutlineHeart size={size} color='#ef4444' />
+          )}
         </button>
       </div>
       <div className='col-3'>
         <button className='job-control--button' onClick={handleBookmark}>
-          {bookmark ? <HiBookmark color='#2f80ed' /> : <HiOutlineBookmark color='#2f80ed' />}
+          {bookmark ? (
+            <HiBookmark size={size} color='#2f80ed' />
+          ) : (
+            <HiOutlineBookmark size={size} color='#2f80ed' />
+          )}
         </button>
       </div>
       <div className='col-3'>
         <button className='job-control--button' onClick={handleApplied}>
-          {applied ? <HiCheckCircle color='#4ade80' /> : <HiOutlineCheckCircle color='#4ade80' />}
+          {applied ? (
+            <HiCheckCircle size={size} color='#4ade80' />
+          ) : (
+            <HiOutlineCheckCircle size={size} color='#4ade80' />
+          )}
         </button>
       </div>
       <div className='col-3'>
         <button className='job-control--button' onClick={handleHidden}>
-          {hidden ? <HiEyeOff color='#ffa214' /> : <HiOutlineEyeOff color='#ffa214' />}
+          {hidden ? (
+            <HiEyeOff size={size} color='#ffa214' />
+          ) : (
+            <HiOutlineEyeOff size={size} color='#ffa214' />
+          )}
         </button>
       </div>
     </div>
