cc4f6944fc567812fc8229ab84987f0dd0f2f673
Lucian Tranc <ltranc@uoguelph.ca>
2021-03-27 17:21:34 -0400

    Can now display the job info taken from a javascript object

--- app/src/js/components/JobHeader.js
+++ app/src/js/components/JobHeader.js
@@ -15,26 +15,26 @@ import {HiArrowNarrowLeft, HiExternalLink} from 'react-icons/hi';
  * @returns {ReactElement} The header with all the job's info displayed.
  */
 export default function JobHeader({ job_data }) {
+  const url = new URL(job_data.link);
   return (
     <div className="job-header-div">
       <div className="back-button-section">
         <HiArrowNarrowLeft className="back-button"></HiArrowNarrowLeft>
       </div>
       <div className="job-section-container">
-        <h3>Tulip Retail</h3>
+        <h3>{job_data.company}</h3>
         <div className="job-title-function-group">
-          <h1>Software Developer</h1>
+          <h1>{job_data.title}</h1>
           <div className="job-functions-div">
-            <JobControl/>
+            <JobControl size = '1.5em'/>
           </div>
         </div>
         <div className="original-link-div">
-          <p>
-            <a href = "#">View on indeed</a>&nbsp;&nbsp;&nbsp;
-            <HiExternalLink className="external-link-icon"></HiExternalLink>
-          </p>
+          <a href = "#">View on {url.hostname}</a>&nbsp;&nbsp;&nbsp;
+          <HiExternalLink className="external-link-icon"></HiExternalLink>
         </div>
       </div>
     </div>
+    
   );
 }
