9f18b65f89b09eb6e9bbf6821b208de1398e9b05
AJ F <aferron@uoguelph.ca>
2021-04-05 13:37:06 -0400

    Update components to support backend changes

--- app/src/js/components/JobControl.js
+++ app/src/js/components/JobControl.js
@@ -1,4 +1,6 @@
-import * as React from 'react';
+import React, { useEffect, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import swal from 'sweetalert';
 import {
   HiHeart,
   HiOutlineHeart,
@@ -10,42 +12,76 @@ import {
   HiOutlineEyeOff,
 } from 'react-icons/hi';
 
+import { Session } from '../utils/contexts';
+
 import '../../scss/job-control.scss';
 
-const { useState, useEffect } = React;
 
 // TODO: create a tooltip component or use react-tooltip and wrap the controls around the tooltip. Team discussion required
 
-/**
- * @returns {ReactElement} The Job Card component with job title, company, job details.
+/** JobControl Component
+ * @prop    {String}        jobID   The ID of the associated job
+ * @prop    {Number}        likes   (TBD) The number of likes the job has
+ * @prop    {String}        size    The size of the control icons (measured in em)
+ * @returns {ReactElement}          The Job Card component with job title, company, job details
  */
-export default function JobControls(props) {
-  const [jobID, setJobID] = useState('');
-  const [heart, setheart] = useState(false);
-  const [bookmark, setBookmark] = useState(false);
-  const [applied, setApplied] = useState(false);
-  const [hidden, setHidden] = useState(false);
+function JobControls({jobID, likes, size='1em'}) {
+  const {id: userID, collections, setCollection} = useContext(Session);
+  const history = useHistory();
 
-  const size = props.size ?? '1em';
 
-  const handleHeart = () => {
-    setheart(!heart);
-  };
-  const handleBookmark = () => {
-    setBookmark(!bookmark);
-  };
-  const handleApplied = () => {
-    setApplied(!applied);
-  };
-  const handleHidden = () => {
-    setHidden(!hidden);
-  };
+  useEffect(() => {
+    const state = collections.get(jobID);
+
+    if (collections?.sync === jobID) {
+      if (userID) {
+        fetch(`${process.env.REACT_APP_API_URL}/collections`, {
+          headers: {'Content-Type': 'application/json'},
+          method: 'POST',
+          body: JSON.stringify({user_id: userID, job_id: jobID, state}),
+        })
+          .then((res) => res.json())
+          .then((res) => {
+            if (res.success) {
+              setCollection(jobID, collections.get(jobID));
+            } else {
+              console.log(res.error);
+            }
+          })
+          .catch((err) => {
+            // Display an error popup
+            console.error(err);
+          });
+      } else {
+        setCollection(jobID, {});
+
+        swal({
+          icon: 'warning', title: 'Uh oh!', text: 'You must be logged in to use collections',
+          buttons: {
+            confirm: {text: 'Sign up!', value: true},
+            cancel: {text: 'Cancel', value: false, visible: true}
+          }
+        })
+          .then((value) => {
+            if (value) history.push('/login');
+          });
+      }
+    }
+  }, [collections]);
+
+
+  function setState(collection, e) {
+    const c = collections.get(jobID);
+    setCollection(jobID, ({...c, [collection]: !c?.[collection]}), jobID);
+    e.preventDefault();
+  }
+
 
   return (
-    <div className='row'>
-      <div className='col-3'>
-        <button className='job-control--button' onClick={handleHeart}>
-          {heart ? (
+    <div className='job-controls row'>
+      <div className='col-3 ms-0'>
+        <button className='job-control--button' onClick={(e) => setState('liked', e)}>
+          {collections.get(jobID)?.liked ? (
             <HiHeart size={size} color='#ef4444' />
           ) : (
             <HiOutlineHeart size={size} color='#ef4444' />
@@ -53,8 +89,8 @@ export default function JobControls(props) {
         </button>
       </div>
       <div className='col-3'>
-        <button className='job-control--button' onClick={handleBookmark}>
-          {bookmark ? (
+        <button className='job-control--button' onClick={(e) => setState('bookmarked', e)}>
+          {collections.get(jobID)?.bookmarked ? (
             <HiBookmark size={size} color='#2f80ed' />
           ) : (
             <HiOutlineBookmark size={size} color='#2f80ed' />
@@ -62,8 +98,8 @@ export default function JobControls(props) {
         </button>
       </div>
       <div className='col-3'>
-        <button className='job-control--button' onClick={handleApplied}>
-          {applied ? (
+        <button className='job-control--button' onClick={(e) => setState('applied', e)}>
+          {collections.get(jobID)?.applied ? (
             <HiCheckCircle size={size} color='#4ade80' />
           ) : (
             <HiOutlineCheckCircle size={size} color='#4ade80' />
@@ -71,8 +107,8 @@ export default function JobControls(props) {
         </button>
       </div>
       <div className='col-3'>
-        <button className='job-control--button' onClick={handleHidden}>
-          {hidden ? (
+        <button className='job-control--button' onClick={(e) => setState('hidden', e)}>
+          {collections.get(jobID)?.hidden ? (
             <HiEyeOff size={size} color='#ffa214' />
           ) : (
             <HiOutlineEyeOff size={size} color='#ffa214' />
@@ -82,3 +118,5 @@ export default function JobControls(props) {
     </div>
   );
 }
+
+export default JobControls;
