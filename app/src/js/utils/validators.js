c54e18d6cdc4e1add136973e1c834c56b1362746
Niloy Kumar Ghosh <55622147+niloyKGhosh@users.noreply.github.com>
2021-04-05 10:35:24 -0400

    task/create profile tab (#51)
    * Profile Tab 85% done
    
    * Completed with form submission setup

--- app/src/js/utils/validators.js
+++ app/src/js/utils/validators.js
@@ -1,8 +1,20 @@
 import * as yup from 'yup';
 
+const emailValidation = yup.string().email('enter a valid email').required('(required)');
+const firstNameValidation = yup
+  .string()
+  .required('(required)')
+  .min(2, '(must be ateast 2 character)');
+const lastNameValidation = yup
+  .string()
+  .required('(required)')
+  .min(2, '(must be ateast 2 character)');
+const urlValidation = yup.string().trim().notRequired().url('(must be a valid url)');
+const normalValidation = yup.string().trim().notRequired();
+
 // validation schema for login page
 export const loginValidationSchema = yup.object().shape({
-  email: yup.string().email('enter a valid email').required('(required)'),
+  email: emailValidation,
   password: yup
     .string()
     .required('(required)')
@@ -11,3 +23,27 @@ export const loginValidationSchema = yup.object().shape({
       '(must be minimum 8 characters, at least 1 letter and 1 number)'
     ),
 });
+
+export const profileValidationCandidateSchema = yup.object().shape({
+  firstName: firstNameValidation,
+  lastName: lastNameValidation,
+  email: emailValidation,
+  github: urlValidation,
+  education: normalValidation,
+  degree: normalValidation,
+  github: urlValidation,
+  linkedIn: urlValidation,
+  personalSite: urlValidation,
+  receiveNotification: yup.boolean(),
+  makeVisibleEmployers: yup.boolean(),
+  startDate: yup.date().notRequired(),
+  endDate: yup.date().notRequired().when('startDate', (startDate, schema) => (startDate && schema.min(startDate, '(cannot be smaller than start date)')))
+});
+
+export const profileValidationEmployerSchema = yup.object().shape({
+  firstName: firstNameValidation,
+  lastName: lastNameValidation,
+  email: emailValidation,
+})
+
+export const zeroValidationSchema = yup.object().shape({});
